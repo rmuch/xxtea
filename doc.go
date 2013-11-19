892de8268680f73732be5b91ac0bd0781659ff23
Rupert Muchembled <rupert@rmuch.net>
2013-11-19 13:11:36 +0000

    Improve package documentation.

--- doc.go
+++ doc.go
@@ -1,2 +1,13 @@
-// Package xxtea implements the Corrected Block TEA cipher.
+/*
+Package xxtea implements the Corrected Block TEA cipher.
+
+The Corrected Block TEA cipher was described in Needham and Wheeler's 1998
+technical report "Correction to xtea", following on from their 1997 technical
+report "Tea extensions".
+
+Correction to xtea (October 1998) - http://www.movable-type.co.uk/scripts/xxtea.pdf
+
+Tea extensions (October 1997) - http://www.movable-type.co.uk/scripts/xtea.pdf
+
+*/
 package xxtea
